4654f25c5b0a153b124ae1599eb55258f4c6a3d8
Mehmet OGUT <mogut44949@gmail.com>
2024-05-14 12:09:07 +0300

    eventCopyCut

--- src/component/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ src/component/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -6,19 +6,24 @@ const KeyboardClipboard = () => {
   const  [icerik, setIcerik] = useState("")
 
   const handleKeyDown=(e)=>{
-    
     console.log(e)
     if(e.keyCode>47 && e.keyCode<58){
       alert("cannot entered number")
       e.preventDefault()// girilen sayının default olarak bağlanması engellendi
+    } }
+    
+    const handleCopy=(e)=>{
+      alert("kopyalandı")
     }
-
-  }
   
+    const handleCut=(e)=>{
+      alert("kesmek yasak")
+      e.preventDefault()// kesimeyi engellemek için kullandım 
+    }
   
   
   return (
-    <div className="container">
+    <div className="container" onCut={handleCut}>
 
 
       <h2>Keyboard Event</h2>
@@ -33,7 +38,11 @@ const KeyboardClipboard = () => {
 
       <div className="mt-3">
         {/*burası hep kücük harf diğeri ise her zamn büyük harf olacaktır */}
-        <p>{icerik.toLocaleLowerCase()}</p>
+        <p onCopy={handleCopy}  >{icerik.toLocaleLowerCase()}</p>
+      </div>
+
+      <div>
+        <textarea className="form-control" name="textarea" id="area" cols="30" rows="10"></textarea>
       </div>
     </div>
   )
