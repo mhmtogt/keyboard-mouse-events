8e829f0c544fdacd757c2e0926f4fe9e97d91fb5
Mehmet OGUT <mogut44949@gmail.com>
2024-05-14 09:55:19 +0300

    formsObject

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import Forms from "./component/1-forms/Forms";
+import FormsObject from "./component/1-forms/FormsObject";
+//import Forms from "./component/1-forms/Forms";
 // import MouseEvent from "./component/3-mouseEvents/MouseEvent";
 // import KeyboardClipboard from "./component/2-keyboardClipboardEvents/KeyboardClipboard";
 
 const App = () => {
   return (
     <div>
-      <Forms />
-      {/* <KeyboardClipboard />
+      <FormsObject />
+      {/*  <Forms />
+    <KeyboardClipboard />
       <MouseEvent /> */}
     </div>
   );
